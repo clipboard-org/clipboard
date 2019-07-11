450204044a82ea5f4dbcc3c567de49f95019d968
AtLeastITry <matthew.hope396@outlook.com>
2019-07-11 21:38:54 +0100

    Added tests for xml.

---
+++ src/Clipboard.Text/XmlReader.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+using Clipboard.Abstraction;
+
+namespace Clipboard.Text
+{
+    public class XmlReader : IDocumentReader
+    {
+        public string Read(FileStream fileStream)
+        {
+            var doc = new XmlDocument();
+            doc.Load(fileStream);
+            return doc.InnerText;
+        }
+
+        public Task<string> ReadAsync(FileStream fileStream)
+        {
+            return Task.FromResult(Read(fileStream));
+        }
+    }
+}
--- src/Clipboard/DocumentReaderFactory.cs
+++ src/Clipboard/DocumentReaderFactory.cs
@@ -1,6 +1,9 @@
-﻿using Clipboard.Abstraction;
+﻿using System.Runtime.InteropServices;
+using Clipboard.Abstraction;
 using Clipboard.IFilter;
-using System.Runtime.InteropServices;
+using Clipboard.OpenXml;
+using Clipboard.Pdf;
+using Clipboard.Text;
 
 namespace Clipboard
 {
@@ -11,11 +14,11 @@ namespace Clipboard
             switch(contentType)
             {
                 case ContentTypeNames.Application.Pdf:
-                    return new Pdf.PdfReader();
+                    return new PdfReader();
                 case ContentTypeNames.Application.Docx:
                 case ContentTypeNames.Application.Dotx:
                 case ContentTypeNames.Application.Dotm:
-                    return new OpenXml.WordReader();
+                    return new WordReader();
                 case ContentTypeNames.Application.Xls :
                 case ContentTypeNames.Application.Xlsx:
                 case ContentTypeNames.Application.Xltx:
@@ -23,7 +26,7 @@ namespace Clipboard
                 case ContentTypeNames.Application.Xltm:
                 case ContentTypeNames.Application.Xlam:
                 case ContentTypeNames.Application.Xlsb:
-                    return new OpenXml.ExcelReader();
+                    return new ExcelReader();
                 case ContentTypeNames.Application.Ppt :
                     throw new System.Exception("Unsupported file type");
                 case ContentTypeNames.Application.Pptx:
@@ -33,13 +36,15 @@ namespace Clipboard
                 case ContentTypeNames.Application.Pptm:
                 case ContentTypeNames.Application.Potm:
                 case ContentTypeNames.Application.Ppsm:
-                    return new OpenXml.PowerpointReader();
+                    return new PowerpointReader();
                 case ContentTypeNames.Text.Plain:
-                    return new Text.PlainReader();
+                    return new PlainReader();
                 case ContentTypeNames.Text.Csv:
-                    return new Text.CsvReader();
+                    return new CsvReader();
                 case ContentTypeNames.Text.Html:
-                    return new Text.HtmlReader();
+                    return new HtmlReader();
+                case ContentTypeNames.Text.Xml:
+                    return new XmlReader();
                 default:
                     if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                         return new IFilterReader();
