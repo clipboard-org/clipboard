bc3a38385f72519f869f61b4f355417846f2889e
Matt Hope <matthew.hope396@outlook.com>
2019-07-22 13:38:23 +0100

    Fixed issues with excel extraction and updated nuspec.

---
+++ samples/QuickStart/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using Clipboard;
+
+namespace QuickStart
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var filePath = $"{Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)}\\example.pdf";
+
+            using (var extractor = TextExtractor.Open(filePath))
+            {
+                Console.WriteLine(await extractor.ExtractAsync());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- src/Clipboard.OpenXml/ExcelReader.cs
+++ src/Clipboard.OpenXml/ExcelReader.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.Packaging;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Clipboard.Abstraction;
@@ -15,12 +17,27 @@ namespace Clipboard.OpenXml
             var sb = new StringBuilder();
             using(var document = SpreadsheetDocument.Open(Package.Open(fileStream)))
             {
-                foreach(var sheet in document.WorkbookPart.WorksheetParts)
-                {
+                var stringValues = document.WorkbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ToArray();
+
+                foreach (var sheet in document.WorkbookPart.WorksheetParts)
+                {    
                     foreach(var cell in sheet.Worksheet.Descendants<Cell>())
                     {
-                        sb.Append(cell.CellFormula?.Text ?? cell.CellValue.Text);
-                        sb.Append(" ");
+                        if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+                        {
+                            var index = int.Parse(cell.CellValue.Text);
+                            sb.Append(stringValues[index].InnerText);                            
+                        }
+                        else if(cell.CellFormula != null)
+                        {
+                            sb.Append(cell.CellFormula.Text);
+                        }
+                        else
+                        {
+                            sb.Append(cell.CellValue.Text);
+                        }
+
+                        sb.Append(Environment.NewLine);
                     }
                 }
             }
